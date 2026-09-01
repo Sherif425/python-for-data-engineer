1944dfa4a635cea2c7fe6608de53a815870d49c7
Sherif Mohamed <sherif425@gmail.com>
2026-09-01 16:00:25 +0300

    adding excel version of the roadmap

---
+++ phase-1/week2/drill5.py
@@ -0,0 +1,73 @@
+orders = [
+    {"order_id": 101, "customer_id": 1, "amount": 500},
+    {"order_id": 102, "customer_id": 2, "amount": 750},
+    {"order_id": 103, "customer_id": 1, "amount": 300},
+    {"order_id": 104, "customer_id": 3, "amount": 1200},
+]
+
+customers = [
+    {"customer_id": 1, "name": "Ahmed"},
+    {"customer_id": 2, "name": "Mona"},
+    {"customer_id": 3, "name": "Omar"},
+]
+
+# Task 1 — Build a customer lookup
+
+# Transform:
+
+# customers
+
+# into:
+
+# {
+#     1: "Ahmed",
+#     2: "Mona",
+#     3: "Omar"
+# }
+customer_lookup = {customer["customer_id"]: customer["name"] for customer in customers}
+print("Customer Lookup:", customer_lookup)
+# Task 2 — Enrich the orders
+
+# Create:
+
+# enriched_orders
+
+# containing:
+
+# [
+#     {"order_id": 101, "customer_id": 1, "customer_name": "Ahmed", "amount": 500},
+#     {"order_id": 102, "customer_id": 2, "customer_name": "Mona", "amount": 750},
+#     {"order_id": 103, "customer_id": 1, "customer_name": "Ahmed", "amount": 300},
+#     {"order_id": 104, "customer_id": 3, "customer_name": "Omar", "amount": 1200}
+# ]
+
+enriched_orders = []
+for order in orders:
+    customer_id = order["customer_id"]
+    customer_name = customer_lookup.get(customer_id, "Unknown")
+    enriched_order = {
+        "order_id": order["order_id"],
+        "customer_id": customer_id,
+        "customer_name": customer_name,
+        "amount": order["amount"]
+    }
+    enriched_orders.append(enriched_order)
+
+print("Enriched Orders:", enriched_orders)
+
+for enriched_order in enriched_orders:
+    print(f"Order ID: {enriched_order['order_id']}, Customer Name: {enriched_order['customer_name']}, Amount: {enriched_order['amount']}")
+
+
+prices = {order["order_id"]: order["amount"] for order in orders}
+print("Prices:", prices)
+total = {}
+
+for id, price in prices.items():
+    if price is not None:
+        if id in total:
+            total[id] += price
+        else:
+            total[id] = price
+    
+print("Total:", total)
